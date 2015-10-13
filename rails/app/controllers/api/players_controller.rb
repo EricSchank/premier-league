b123773d79b6bd7a2a5f95e4fdf9833ae0b490e1
Eric Schank <eric.schank@gmail.com>
2015-10-12 22:46:29 -0500

    fix caching - cache results, not the AREL object

--- rails/app/controllers/api/players_controller.rb
+++ rails/app/controllers/api/players_controller.rb
@@ -3,9 +3,9 @@ class Api::PlayersController < ApplicationController
     id = params[:id]
     players = Rails.cache.fetch("/players/team/#{id}", expires_in: 1.day) do
       if id.blank?
-        Player.all
+        Player.all.includes(:team).to_a
       else
-        Player.where(team_id: id)
+        Player.where(team_id: id).includes(:team).to_a
       end
     end
     render json: players
@@ -14,7 +14,7 @@ class Api::PlayersController < ApplicationController
   def show
     id = params[:id]
     player = Rails.cache.fetch("/player/#{id}") do
-      Player.where(id: id)
+      Player.where(id: id).includes(:team)
     end
     render json: player
   end
--- rails/app/controllers/api/teams_controller.rb
+++ rails/app/controllers/api/teams_controller.rb
@@ -2,7 +2,7 @@ class Api::TeamsController < ApplicationController
   def index
     teams = Rails.cache.fetch('/teams', expires_in: 1.day) do
       Team.refresh
-      Team.all
+      Team.all.includes(:players).to_a
     end
     render json: teams
   end
@@ -10,8 +10,9 @@ class Api::TeamsController < ApplicationController
   def show
     id = params[:id]
     team = Rails.cache.fetch("/team/#{id}", expires_in: 1.day) do
-      cur_team = Team.where(id: id)
+      cur_team = Team.where(id: id).includes(:players).first
       Player.refresh(cur_team)
+      cur_team
     end
     render json: team
   end
