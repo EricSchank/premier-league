700d56f7aa5dcd0ac2dc214157fb1bf2ab16f801
Eric Schank <eric.schank@gmail.com>
2015-10-12 22:45:07 -0500

    use latest beta of AMS

--- rails/app/serializers/player_serializer.rb
+++ rails/app/serializers/player_serializer.rb
@@ -1,3 +1,5 @@
 class PlayerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :position, :jersey_number, :birth, :value, :currency, :nationality, :contract
+  attributes :id, :name, :position, :birth, :value, :currency, :nationality, :contract
+  attribute :jersey_number, :key => 'jersey-number'
+  belongs_to :team
 end
--- rails/app/serializers/team_serializer.rb
+++ rails/app/serializers/team_serializer.rb
@@ -1,3 +1,5 @@
 class TeamSerializer < ActiveModel::Serializer
-  attributes :id, :name, :short_name, :crest, :code, :value, :currency
+  attributes :id, :name, :crest, :code, :value, :currency
+  has_many :players
+  attribute :short_name, key: 'short-name'
 end
